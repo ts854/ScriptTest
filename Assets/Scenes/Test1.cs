218adae4600a122653cf0262cce482bd3bee65b2
ts854 <76829034+ts854@users.noreply.github.com>
2021-02-10 23:17:45 +0900

    Initial commit

---
+++ Assets/Scenes/Test1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test1: MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // 配列を初期化する
+        int[] points = { 10, 20, 30, 40, 50};
+
+        // 配列の要素数のぶんだけ処理を繰り返す
+        for (int i = 0; i < points.Length; i++)
+        
+            {
+                // 配列の要素を表示する
+                Debug.Log(points[i]);
+          
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scenes/Test2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test2: MonoBehaviour
+{
+
+    void Start()
+    {
+        int[] points = { 10, 20, 30, 40, 50 };
+        for (int i = 4; i >= 0; i--)
+
+        {
+            Debug.Log(points[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
